bddc1dde03b9f9d14dff23db140106a455c6fed9
Venício <vr.rneto@gmail.com>
2026-06-01 21:31:24 -0300

    Delete editor.js

--- editor.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('imgUpload').addEventListener('change',e=>{
-const file=e.target.files[0];
-if(!file)return;
-const r=new FileReader();
-r.onload=x=>document.getElementById('foto').src=x.target.result;
-r.readAsDataURL(file);
-});
